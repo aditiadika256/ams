078ffdcbb9d7f2c501d46902bf0163a772349ad0
Aditia Dika <aditiadika256@gmail.com>
2026-07-17 14:31:07 +0800

    C Mentor - Perubahan menjadi LoV user

--- apps/api/app/Domain/Admin/UserController.php
+++ apps/api/app/Domain/Admin/UserController.php
@@ -59,6 +59,21 @@ class UserController extends Controller
             });
         }
 
+        if ($request->has('fields')) {
+            $fields = collect(explode(',', $request->query('fields')))
+                ->map(fn($field) => trim($field))
+                ->filter()
+                ->unique()
+                ->values()
+                ->all();
+
+            if (!empty($fields)) {
+                $query->select($fields);
+            }
+        } else {
+            $query->with(['roles', 'branch']);
+        }
+
         $users = $query->latest()
             ->paginate($this->getLimit($request));
 
